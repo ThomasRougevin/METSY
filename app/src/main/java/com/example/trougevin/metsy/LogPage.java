0231c425d67923aaf676d87fe17c4ea30b1f173c
ThomasRougevin <rougevinbaville.thomas@gmail.com>
2018-11-16 10:36:28 +0100

    test 2 commit

--- app/src/main/java/com/example/trougevin/metsy/LogPage.java
+++ app/src/main/java/com/example/trougevin/metsy/LogPage.java
@@ -8,6 +8,7 @@ public class LogPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_page);
     }
