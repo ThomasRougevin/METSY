fdbf504950ad26b77e70c1fd975676114e2782f0
Thomas ROUGEVIN <TROUGEVIN@treville.mousquetaires.grp>
2018-11-16 09:47:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/trougevin/metsy/LogPage.java
@@ -0,0 +1,18 @@
+package com.example.trougevin.metsy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LogPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_page);
+    }
+
+    public void connexion_click (View view){
+
+    }
+}
