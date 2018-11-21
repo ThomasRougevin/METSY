c5afed6f791ecd7e42065d0d234d1087171a5055
deloynes <alienor.de.loynes@fr.ibm.com>
2018-11-21 15:09:05 +0100

    Aliénor BarcodeScanner

---
+++ app/src/main/java/com/example/trougevin/metsy/BarcodeScanner.java
@@ -0,0 +1,13 @@
+package com.example.trougevin.metsy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarcodeScanner extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scanner);
+    }
+}
