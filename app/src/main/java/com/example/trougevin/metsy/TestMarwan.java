750a75a84b98b46fe7c56d016333b07cb7cce53e
BMarwan <marwan.boughzala@isep.fr>
2018-11-21 14:19:16 +0100

    test marwan

---
+++ app/src/main/java/com/example/trougevin/metsy/TestMarwan.java
@@ -0,0 +1,13 @@
+package com.example.trougevin.metsy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestMarwan extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_marwan);
+    }
+}
